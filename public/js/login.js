1e6ee916e77d50c91ba7b60ffdaa4bb47613bde0
DannyM15 <merinodanny15@gmail.com>
2024-04-09 21:05:28 -0700

    bug1

--- public/js/login.js
+++ public/js/login.js
@@ -9,13 +9,13 @@ const loginFormHandler = async (event) => {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/reivews');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/reviews');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
@@ -48,6 +48,5 @@ const loginFormHandler = async (event) => {
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  
+  
